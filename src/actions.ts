8a6ca5cc38e47d69f8c115f8bd11c5e0f2aebec0
michaelfarrell76 <mike@transcend.io>
2022-01-23 23:39:23 -0800

    Adds RequestActionObjectResolver

--- src/actions.ts
+++ src/actions.ts
@@ -19,6 +19,39 @@ export type RequestActionOptOut =
   typeof RequestActionOptOut[keyof typeof RequestActionOptOut];
 
 /**
+ * An request action resolve types that can be run at the object level
+ */
+export const RequestActionObjectResolver = makeEnum({
+  /** Data Download request */
+  Access: 'ACCESS',
+  /** Erase the file completely */
+  Erasure: 'ERASURE',
+  /** Run an account deletion instead of a fully compliant deletion */
+  AccountDeletion: 'ACCOUNT_DELETION',
+  /** Opt out of automated decision making */
+  AutomatedDecisionMakingOptOut: 'AUTOMATED_DECISION_MAKING_OPT_OUT',
+  /** A contact opt out request */
+  ContactOptOut: 'CONTACT_OPT_OUT',
+  /** Opt-out of the sale of personal data */
+  SaleOptOut: 'SALE_OPT_OUT',
+  /** A tracking opt out request */
+  TrackingOptOut: 'TRACKING_OPT_OUT',
+  /** Make an update to an inaccurate record */
+  Rectification: 'RECTIFICATION',
+  /** A restriction of processing request */
+  Restriction: 'RESTRICTION',
+  /**
+   * TODO: https://transcend.height.app/T-4327 - add in the ability to choose ERASURE vs REDACT
+   * --  Redact the personal data from the file, without deleting it entirely
+   */
+  // Redact: 'REDACT',
+});
+
+/** Type override */
+export type RequestActionObjectResolver =
+  typeof RequestActionObjectResolver[keyof typeof RequestActionObjectResolver];
+
+/**
  * The types of requests that Data Subject can make
  */
 export const RequestAction = makeEnum({
