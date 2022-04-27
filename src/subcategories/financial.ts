e19924447b67d5a3ecc1a4ea696702d5ba4a0a88
anotherminh <minh@transcend.io>
2022-04-27 12:56:50 -0400

    Standardize casing for enums

--- src/subcategories/financial.ts
+++ src/subcategories/financial.ts
@@ -1,11 +1,11 @@
 import { makeEnum } from '@transcend-io/type-utils';
 
 export const FinancialSubCategory = makeEnum({
-  /** Credit Card Number */
-  CreditCardNumber: 'creditCardNumber',
   // TODO: https://transcend.height.app/T-14003 - add more financial subcategories (routing number, etc.)
   /** Fallback subcategory */
   Financial: 'FINANCIAL',
+  /** Credit Card Number */
+  CreditCardNumber: 'CREDIT_CARD_NUMBER',
 });
 
 /**
