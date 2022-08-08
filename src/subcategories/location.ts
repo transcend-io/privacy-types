29951e276d505ac1dfc73e0069fe4c72b8ad13a4
anotherminh <minhngoc.ln@gmail.com>
2022-08-08 15:28:31 -0400

    Remove deprecated subcategories

--- src/subcategories/location.ts
+++ src/subcategories/location.ts
@@ -1,24 +1,6 @@
 import { makeEnum } from '@transcend-io/type-utils';
 
 export const LocationSubCategory = makeEnum({
-  /**
-   * An address where an individual is located
-   *
-   * @deprecated
-   */
-  PhysicalAddress: 'PHYSICAL_ADDRESS',
-  /**
-   * Exact coordinates for an individual's location
-   *
-   * @deprecated
-   */
-  PreciseGeolocation: 'PRECISE_GEOLOCATION',
-  /**
-   * Approximate area where an individual is located
-   *
-   * @deprecated
-   */
-  ApproximateLocation: 'APPROXIMATE_LOCATION',
   /** Fallback subcategory */
   Location: 'LOCATION',
 });
