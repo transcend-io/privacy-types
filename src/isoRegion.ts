f2a71607ce7abc0c29a304fbe32839ca72675226
linh-transcend <linh@transcend.io>
2022-08-10 15:22:57 -0400

    add return type

--- src/isoRegion.ts
+++ src/isoRegion.ts
@@ -9,7 +9,9 @@ import { ISO_31662 } from './isoConstants/iso3166-2';
  * @param source - the source lookup
  * @returns the typed dictionary
  */
-function getDictFromKeys<T>(source: T) {
+function getDictFromKeys<T>(source: T): {
+  [K in keyof T]: K;
+} {
   return Object.fromEntries(
     Object.keys(source).map((code) => [code, code]),
   ) as {
