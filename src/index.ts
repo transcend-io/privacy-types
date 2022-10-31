68d036983802a66d4b4a77fc2c73523ccc0a3d9e
Alessandro Gagliardi <alessandro@transcend.io>
2022-10-31 15:57:39 -0400

    export macroregion constants (#56)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './actions';
 export * from './objects';
 export * from './request';
 export * from './identifier';
+export * from './macroregion';
 export * from './subcategories';
 export * from './purposeSubCategories';
 export * from './promptAVendor';
