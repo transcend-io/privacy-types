dcb3fb91e8d681a656a4a6bf841328f1bc3a14e5
Arthur <abrantesarthur1997@gmail.com>
2023-11-01 23:14:48 +0000

    create assessmentForm statuses

---
+++ src/assessmentForm.ts
@@ -0,0 +1,15 @@
+/** AssessmentForm status types */
+export enum AssessmentFormStatus {
+    /** The form has been created from a template and shared to be filled out */
+  Shared = 'SHARED',
+  /** The form is in process of being answered */
+  InProgress = 'IN_PROGRESS',
+  /** The form has been fully answered and is being reviewed */
+  InReview = 'IN_REVIEW',
+  /** The reviewers requested changes to the answers */
+  ChangesRequested = 'CHANGES_REQUESTED',
+  /** The reviewers rejected the answers */
+  Rejected = 'REJECTED',
+  /** The reviewers approved the answers */
+  Approved = 'APPROVED',
+}
