a1b867d7b5c2fcfb5c6afb359c3febc5f110a2e5
Michael Farrell <mike@transcend.io>
2023-12-13 16:58:03 -0800

    Code scanning enums (#139)

---
+++ src/code.ts
@@ -0,0 +1,10 @@
+/**
+ * The types of files that can be used to detect a code package
+ */
+export enum CodePackageType {
+  PackageJson = 'PACKAGE_JSON',
+  RequirementsTxt = 'REQUIREMENTS_TXT',
+  Gradle = 'GRADLE',
+  CocoaPods = 'COCOA_PODS',
+  Pubspec = 'PUBSPEC',
+}
