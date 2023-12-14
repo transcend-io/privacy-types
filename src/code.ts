768b73f667c653ac195372dc67c8be3372f2f72e
Michael Farrell <mike@transcend.io>
2023-12-14 13:24:11 -0800

    Use mkeum (#141)
    * Use mkeum
    
    * prettier
    
    * fixes

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,17 @@
+import { makeEnum } from '@transcend-io/type-utils';
+
 /**
  * The types of files that can be used to detect a code package
  */
-export enum CodePackageType {
-  PackageJson = 'PACKAGE_JSON',
-  RequirementsTxt = 'REQUIREMENTS_TXT',
-  Gradle = 'GRADLE',
-  CocoaPods = 'COCOA_PODS',
-  Pubspec = 'PUBSPEC',
-  Gemfile = 'GEMFILE',
-}
+export const CodePackageType = makeEnum({
+  PackageJson: 'PACKAGE_JSON',
+  RequirementsTxt: 'REQUIREMENTS_TXT',
+  Gradle: 'GRADLE',
+  CocoaPods: 'COCOA_PODS',
+  Pubspec: 'PUBSPEC',
+  Gemfile: 'GEMFILE',
+});
+
+/** Overrides type */
+export type CodePackageType =
+  typeof CodePackageType[keyof typeof CodePackageType];
