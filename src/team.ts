61fe73cbcecb5941915e228495f65de9f53d2b24
Michael Farrell <mike@transcend.io>
2024-02-21 16:53:06 -0800

    Adds enum for default teams (#159)

---
+++ src/team.ts
@@ -0,0 +1,19 @@
+/**
+ * The default team types seeded into each Transcend account
+ * used for mapping of action items
+ */
+export enum StaticTeamType {
+  Engineering = 'ENGINEERING',
+  DsrManagement = 'DSR_MANAGEMENT',
+  ReadOnly = 'READ_ONLY',
+  PrivacyCenterManagement = 'PRIVACY_CENTER_MANAGEMENT',
+  AccessControlManagement = 'ACCESS_CONTROL_MANAGEMENT',
+  Design = 'DESIGN',
+  LegalPrivacyOps = 'LEGAL_PRIVACY_OPS',
+  ItInfoSec = 'IT_INFO_SEC',
+  DatabaseAdminDataEngineering = 'DATABASE_ADMIN_DATA_ENGINEERING',
+  CloudInfraArchitecture = 'CLOUD_INFRA_ARCHITECTURE',
+  ProjectManagers = 'PROJECT_MANAGERS',
+  Marketing = 'MARKETING',
+  SystemOwners = 'SYSTEM_OWNERS',
+}
