86bb8256d45c35d658fd135c63e9d727d5ce8ff7
michaelfarrell76 <mike@transcend.io>
2024-11-26 00:42:24 -0800

    ud

--- src/assessmentForm.ts
+++ src/assessmentForm.ts
@@ -1,4 +1,9 @@
 /** AssessmentForm status types */
+import { makeEnum } from '@transcend-io/type-utils';
+
+/**
+ * AssessmentForm status types
+ */
 export enum AssessmentFormStatus {
   /** The form has been created but has not been shared with assignees */
   Draft = 'DRAFT',
@@ -23,3 +28,19 @@ export enum AssessmentFormTemplateStatus {
   /** The template is ready to be used to create forms */
   Published = 'PUBLISHED',
 }
+
+/** How an assessment form template was created */
+export const AssessmentFormTemplateSource = makeEnum({
+  /** The template was created manually by a user creating sections, questions, etc in the AD or via API  */
+  Manual: 'MANUAL',
+  /** The template was auto-generated as a side effect of kicking off assessments from the Data Inventory */
+  DataInventory: 'DATA_INVENTORY',
+  /** The template was auto-generated as a side effect of importing OneTrust assessments */
+  Import: 'IMPORT',
+});
+
+/**
+ * Overload type
+ */
+export type AssessmentFormTemplateSource =
+  typeof AssessmentFormTemplateSource[keyof typeof AssessmentFormTemplateSource];
