aa4de21454c48d58a5ba853961ac665c57cb7b5c
maxgtranscend <max.gong@transcend.io>
2025-03-04 11:02:57 -0800

    Add identifiers for Adobe Experience Platform (#219)

--- src/identifier.ts
+++ src/identifier.ts
@@ -87,6 +87,18 @@ export const IdentifierType = makeEnum({
   OnfidoApplicantId: 'onfidoApplicantId',
   /** Custom ID for a Vero user */
   VeroUserId: 'veroUserId',
+  /**
+   * Adobe Privacy API ID's
+   * https://experienceleague.adobe.com/en/docs/experience-platform/privacy/api/appendix#standard-namespaces
+   */
+  /** Adobe Advertising Cloud ID */
+  AdobeAdvertisingCloudId: 'adobeAdvertisingCloudId',
+  /** Adobe Audience Manager UUID */
+  AdobeAudienceManagerId: 'adobeAudienceManagerId',
+  /** Adobe Experience Cloud ID */
+  AdobeExperienceCloudId: 'adobeExperienceCloudId',
+  /** Adobe Target ID */
+  AdobeTargetID: 'adobeTargetId',
 });
 
 /**
