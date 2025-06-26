8125027a431c46644bd73e3c6af3351d0965dca4
iamtheluckyest <annalisa@transcend.io>
2025-06-26 17:10:32 -0400

    Change it to t.object

--- src/oneTrust/endpoints/getAssessment.ts
+++ src/oneTrust/endpoints/getAssessment.ts
@@ -213,19 +213,7 @@ export const OneTrustAssessmentResponses = t.array(
     }),
     t.type({
       /** The controlResponse */
-      controlResponse: t.union([
-        t.string,
-        t.type({
-          controlId: t.string,
-          controlIdentifier: t.string,
-          controlName: t.string,
-          frameworkId: t.string,
-          frameworkName: t.string,
-          categoryId: t.string,
-          categoryName: t.string,
-        }),
-        t.null,
-      ]),
+      controlResponse: t.union([t.string, t.object, t.null]),
       /** The relationshipResponseDetails */
       relationshipResponseDetails: t.array(t.string),
       /** The textRedacted */
