e6a9daeabc175caf985bba8b667e0fd5f432ab93
Linh Chau <94718676+linh-transcend@users.noreply.github.com>
2025-09-15 20:03:13 +0000

    update config codecs

--- src/consentUiConfiguration.ts
+++ src/consentUiConfiguration.ts
@@ -1,6 +1,7 @@
 import * as t from 'io-ts';
-import { makeEnum } from '@transcend-io/type-utils';
 
+import { LOCALE_KEY } from '@transcend-io/internationalization';
+import { makeEnum, valuesOf } from '@transcend-io/type-utils';
 
 /** Actions for buttons in modals */
 export const ModalButtonActions = makeEnum({
@@ -30,3 +31,158 @@ export const ButtonType = makeEnum({
 
 /** Type override */
 export type ButtonType = (typeof ButtonType)[keyof typeof ButtonType];
+
+/** Absolute URL string */
+export const AbsoluteUrlString = t.string;
+
+/** Type override */
+export type AbsoluteUrlString = t.TypeOf<typeof AbsoluteUrlString>;
+
+/** Top-level configuration shared between the UI layers */
+export const SharedTopLevelConfig = t.type({
+  /** Supported locales */
+  locales: t.array(valuesOf(LOCALE_KEY)),
+  /** The transcend URL where the transcend logo links to */
+  transcendUrl: AbsoluteUrlString,
+  /** The default language to use - must be one of the supported locales */
+  defaultLanguage: valuesOf(LOCALE_KEY),
+});
+
+/** Type override */
+export type SharedTopLevelConfig = t.TypeOf<typeof SharedTopLevelConfig>;
+
+/** Button configuration */
+export const ButtonConfig = t.type({
+  type: valuesOf(ButtonType),
+  action: valuesOf(ButtonAction),
+});
+
+/** Type override */
+export type ButtonConfigCodec = t.TypeOf<typeof ButtonConfigCodec>;
+
+/** Configuration for buttons in modals */
+export const ModalButtonConfig = t.type({
+  type: valuesOf(ButtonType),
+  action: valuesOf(ModalButtonAction),
+});
+
+/** Type override */
+export type ModalButtonConfig = t.TypeOf<typeof ModalButtonConfig>;
+
+/** Footer configuration */
+export const FooterConfig = t.type({
+  showTranscendBadge: t.boolean,
+  showGpcDetected: t.boolean,
+  showLocalePicker: t.boolean,
+});
+
+/** Type override */
+export type FooterConfig = t.TypeOf<typeof FooterConfig>;
+
+/** Common configuration for both first and second layers */
+export const CommonLayerConfig = t.intersection([
+  t.type({
+    showCloseButton: t.boolean,
+  }),
+  t.partial({
+    logoImageUrl: AbsoluteUrlString,
+    alphaMask: t.boolean,
+    showLanguagePicker: t.boolean,
+    footer: FooterConfig,
+    links: t.array(AbsoluteUrlString),
+  }),
+]);
+
+/** Type override */
+export type CommonLayerConfig = t.TypeOf<typeof CommonLayerConfig>;
+
+/** First layer configuration: Banner */
+export const FirstLayerConfig = t.intersection([
+  CommonLayerConfig,
+  t.type({
+    buttons: t.tuple([
+      ButtonConfig,
+      t.union([ButtonConfig, t.undefined]),
+      t.union([ButtonConfig, t.undefined]),
+    ]),
+  }),
+  t.partial({
+    showHeader: t.boolean,
+  }),
+]);
+
+/** Type override */
+export type FirstLayerConfig = t.TypeOf<typeof FirstLayerConfig>;
+
+/** Second layer configuration: Modal */
+export const SecondLayerConfig = t.intersection([
+  CommonLayerConfig,
+  t.type({
+    buttons: t.tuple([
+      ModalButtonConfig,
+      t.union([ModalButtonConfig, t.undefined]),
+      t.union([ModalButtonConfig, t.undefined]),
+    ]),
+    showBulkActions: t.boolean,
+    closeAfterBulkAction: t.boolean,
+    showPurposeDescriptions: t.boolean,
+    showCookiesLink: t.literal(false),
+    showVendorsLink: t.literal(false),
+    showToggleText: t.boolean,
+    showToggleIcon: t.boolean,
+  }),
+]);
+
+/** Type override */
+export type SecondLayerConfig = t.TypeOf<typeof SecondLayerConfig>;
+
+/** --- Top-level UI Configuration --- */
+/** Banner only configuration */
+export const UIConfigurationBannerOnly = t.intersection([
+  SharedTopLevelConfig,
+  t.type({
+    firstLayerConfig: FirstLayerConfig,
+  }),
+]);
+
+/** Type override */
+export type UIConfigurationBannerOnly = t.TypeOf<
+  typeof UIConfigurationBannerOnly
+>;
+
+/** Modal only configuration */
+export const UIConfigurationModalOnly = t.intersection([
+  SharedTopLevelConfig,
+  t.type({
+    secondLayerConfig: SecondLayerConfig,
+  }),
+]);
+
+/** Type override */
+export type UIConfigurationModalOnly = t.TypeOf<
+  typeof UIConfigurationModalOnly
+>;
+
+/** Banner into modal configuration */
+export const UIConfigurationBannerIntoModal = t.intersection([
+  SharedTopLevelConfig,
+  t.type({
+    firstLayerConfig: FirstLayerConfig,
+    secondLayerConfig: SecondLayerConfig,
+  }),
+]);
+
+/** Type override */
+export type UIConfigurationBannerIntoModal = t.TypeOf<
+  typeof UIConfigurationBannerIntoModal
+>;
+
+/** Configuration of the entire consent UI */
+export const UIConfiguration = t.union([
+  UIConfigurationBannerOnly,
+  UIConfigurationModalOnly,
+  UIConfigurationBannerIntoModal,
+]);
+
+/** Type override */
+export type UIConfiguration = t.TypeOf<typeof UIConfigurationModalOnly>;
